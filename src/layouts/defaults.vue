9a6852ad89755b96efe337dbd55c75ab17638e41
eman <138737633+ibrahem-ghaybour@users.noreply.github.com>
2025-10-02 10:11:36 +0300

    add fetch my

--- src/layouts/defaults.vue
+++ src/layouts/defaults.vue
@@ -6,9 +6,16 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from "vue";
+import { useAuthStore } from "~/stores/auth";
 import AppSidebar from "~/components/layouts/AppSidebar.vue";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import Container from "~/components/layouts/Container.vue";
+const authStore = useAuthStore();
+onMounted(async() => {
+  authStore.fetchMe();
+  console.log('dlkjaslkdjflkadsfdsda')
+});
 </script>
 
 <style></style>
